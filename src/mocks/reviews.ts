a1a7ce1dbed19d17fccd527f14a117f10671281a
Анна Крамер <anna.pro.kramer@gmail.com>
2025-04-17 19:32:07 +0300

    Исправляет ошибки

--- src/mocks/reviews.ts
+++ src/mocks/reviews.ts
@@ -1,79 +1,66 @@
-import { Helmet } from 'react-helmet-async';
-import Header from '../../components/header/header';
-import { OfferType } from '../../types/offer-type';
-import { Link } from 'react-router-dom';
-import { useMemo } from 'react';
-import FavoriteComponent from '../../components/favorite-offer/favorite-offer';
+import { CommentType } from '../types/review-type';
 
-type FavoritesOffers = {
-  propsOffers: OfferType[];
-}
+const AVATAR_URL = 'https://i.pravatar.cc/58';
 
-function FavoritesPage({ propsOffers }: FavoritesOffers): JSX.Element {
-
-  const [{ id }] = propsOffers;
-  const pathCard = `/offer/${id}`;
-
-  const citiesArr = useMemo(() => {
-    const favoriteCities = new Set<string>();
-
-    const favoriteOffers = propsOffers.filter((item) => item.isFavorite);
-
-    favoriteOffers.forEach((item: OfferType) => item.city ? favoriteCities.add(item.city.name) : '');
-
-    return {
-      favoriteCitiesArr: Array.from(favoriteCities),
-      favoriteOffers,
-    };
-  }, [propsOffers]);
-
-  return (
-    <div className= "page" >
-    <Helmet>
-    <title>6 cities.Favorites </title>
-      </Helmet>
-      < Header />
-      <main className="page__main page__main--favorites" >
-        <div className="page__favorites-container container" >
-          <section className="favorites" >
-            <h1 className="favorites__title" > Saved listing </h1>
-              < ul className = "favorites__list" >
-              {
-                citiesArr.favoriteCitiesArr.map((city: string) => (
-                  <li className= "favorites__locations-items" key = { city } >
-                  <div className="favorites__locations locations locations--current" >
-                <div className="locations__item" >
-                <Link className="locations__item-link" to = { pathCard } >
-                <span>{ city } </span>
-                </Link>
-                </div>
-                </div>
-                < div className = "favorites__places" >
-                {
-                  citiesArr.favoriteOffers.filter((item) => item.city?.name === city)
-                    .map((item) => <FavoriteComponent favOffer={ item } key = { item.id } />)
-                }
-                </div>
-                </li>
-                ))
-              }
-                </>
-                </section>
-                </div>
-                </main>
-                < footer className = "footer container" >
-                  <a className="footer__logo-link" href = "main.html" >
-                    <img
-            className="footer__logo"
-  src = "img/logo.svg"
-  alt = "6 cities logo"
-  width = { 64}
-  height = { 33}
-    />
-    </a>
-    </footer>
-    </div>
-  );
-}
-
-export default FavoritesPage;
+export const reviews: CommentType[] = [
+  {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    date: '2023-03-02T09:23:20.316Z',
+    id: 1,
+    rating: 2,
+    user: {
+      avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+      id: 1,
+      isPro: false,
+      name: 'Oliver.conner'
+    }
+  },
+  {
+    comment: 'A quiet  a a river by the unique lightness of Amsterdam.',
+    date: '2023-07-02T09:23:20.316Z',
+    id: 2,
+    rating: 5,
+    user: {
+      avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+      id: 1,
+      isPro: false,
+      name: 'Luka.conner'
+    }
+  },
+  {
+    comment: 'A quiet cozy and picturesque by the unique lightness of Amsterdam.',
+    date: '2023-08-02T09:23:20.316Z',
+    id: 3,
+    rating: 3,
+    user: {
+      avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+      id: 1,
+      isPro: true,
+      name: 'Oliver.conner'
+    }
+  },
+  {
+    comment: 'A quiet cozy  a river by the unique lightness of Amsterdam.',
+    date: '2023-10-02T09:23:20.316Z',
+    id: 4,
+    rating: 4,
+    user: {
+      avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+      id: 1,
+      isPro: true,
+      name: 'Nick.conner'
+    }
+  },
+  {
+    comment: 'A quiet cozy and picturesque that hides behind ',
+    date: '2023-11-02T09:23:20.316Z',
+    id: 5,
+    rating: 1,
+    user: {
+      avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+      id: 1,
+      isPro: false,
+      name: 'Valera.conner'
+    }
+  },
+];
