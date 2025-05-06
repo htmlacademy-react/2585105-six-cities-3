d04f5fb61d4c13e11724b9f10ce22abb37da970f
Анна Крамер <anna.pro.kramer@gmail.com>
2025-05-06 22:20:32 +0300

    исправляет линтер

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -14,36 +14,36 @@ export default function Reviews({ reviewsProp }: ReviewType) {
       <h2 className="reviews__title">Reviews · <span className="reviews__amount">{reviewsProp.length}</span></h2>
       <ul className="reviews__list">
         {sortReview.slice(0, 10).map(({ rating, id, user, comment, date }) =>
-        (
-          <li className="reviews__item" key={id}>
-            <div className="reviews__user user">
-              <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                <img
-                  className="reviews__avatar user__avatar"
-                  src={user.avatarUrl}
-                  width={54}
-                  height={54}
-                  alt="Reviews avatar"
-                />
-              </div>
-              <span className="reviews__user-name">{user.name}</span>
-            </div>
-            <div className="reviews__info">
-              <div className="reviews__rating rating">
-                <div className="reviews__stars rating__stars">
-                  <span style={{ width: calculateRating(rating) }} />
-                  <span className="visually-hidden">Rating</span>
+          (
+            <li className="reviews__item" key={id}>
+              <div className="reviews__user user">
+                <div className="reviews__avatar-wrapper user__avatar-wrapper">
+                  <img
+                    className="reviews__avatar user__avatar"
+                    src={user.avatarUrl}
+                    width={54}
+                    height={54}
+                    alt="Reviews avatar"
+                  />
                 </div>
+                <span className="reviews__user-name">{user.name}</span>
               </div>
-              <p className="reviews__text">
-                {comment}
-              </p>
-              <time className="reviews__time" dateTime={date}>
-                {formatDateComment(date)}
-              </time>
-            </div>
-          </li>
-        ))}
+              <div className="reviews__info">
+                <div className="reviews__rating rating">
+                  <div className="reviews__stars rating__stars">
+                    <span style={{ width: calculateRating(rating) }} />
+                    <span className="visually-hidden">Rating</span>
+                  </div>
+                </div>
+                <p className="reviews__text">
+                  {comment}
+                </p>
+                <time className="reviews__time" dateTime={date}>
+                  {formatDateComment(date)}
+                </time>
+              </div>
+            </li>
+          ))}
       </ul>
     </>
 
